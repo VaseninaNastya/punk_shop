3c458037b9b05cfa0d73ac6ef13dd6e96a646e8e
VaseninaNastya <dadasiegt@yandex.ru>
2021-09-20 00:32:33 +0300

    init: init testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReduser from './reducers';
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const store = createStore(rootReduser);
+  render(  
+  <Provider store={store}>
+    <App />
+  </Provider>);
+  const linkElement = screen.getByText(/test/i);
   expect(linkElement).toBeInTheDocument();
 });
