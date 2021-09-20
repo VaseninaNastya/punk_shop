ad27321b99cd8b8a0831893c5c014fde3f411ba5
VaseninaNastya <dadasiegt@yandex.ru>
2021-09-20 23:37:02 +0300

    fix: fix shadow

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,14 +3,22 @@ import s from './Card.module.scss'
 import cl from "classnames";
 
 const Card = (props) => {
-    const {id, name, tagline} = props
+    const {id, name, tagline, description, image_url, first_brewed, volume, abv} = props
     return (
         <div className = {s.productCard}>
-            <ul>
-                <li>{id}</li>
-                <li>{name}</li>
-                <li>{tagline}</li>
+            <h4  className = {s.productCard_title}>{name}</h4>
+            <div className = {s.productCard_headInfo}>
+
+            <img className = {s.productCard_img} src={image_url}/>
+            <ul className = {s.productCard_list}>
+                <li><span>Tagline: </span>{tagline}</li>
+                <li><span>First brewed: </span>{first_brewed}</li>
+                <li><span>Volume: </span>{volume.value} {volume.unit}</li>
+                <li><span>abv: </span>{abv}%</li>
             </ul>
+            </div>
+
+            <p className = {s.productCard_description}> <span>Description: </span>{description}</p>
         </div>
     )
 }
