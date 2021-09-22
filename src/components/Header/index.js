48cbc9ddd907f81566989b301ceee2a65d5bfdc2
VaseninaNastya <dadasiegt@yandex.ru>
2021-09-22 16:40:26 +0300

    fix: fix ctyle

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import s from "./Header.module.scss";
 import { Link } from "react-router-dom";
 const Header = (props) => {
-    const {button, link} = props
+  const { button, link } = props;
   return (
     <div className={s.container}>
-      <h2>Punk Shop</h2>
-      <Link to={link}>{button}</Link>
-
+      <header>
+        <h2>Punk Shop</h2>
+        <Link to={link}>
+            <div className={s.header_button} >{button} </div>  
+        </Link>
+      </header>
     </div>
   );
 };
