2849d35db317ddbc4a3e4d4cb7e23379c032c2aa
VaseninaNastya <dadasiegt@yandex.ru>
2021-09-23 15:16:54 +0300

    fix: fix pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,23 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 import s from "./Pagination.module.scss";
-import PaginationItem from '../PaginationItem/index'
-import PaginationButton from '../PaginationButton'
-import { useDispatch, useSelector } from "react-redux";
+import PaginationItem from "../PaginationItem/index";
+import PaginationButton from "../PaginationButton";
+import { useSelector } from "react-redux";
 
 const Pagination = () => {
-  const activePage = useSelector((state) => state.activePage.activePage);
-  const numbersOfPage = useSelector((state) => state.numbersOfPage.numbersOfPage);
-  const fragment = []
-for (let index = 1; index <= numbersOfPage; index++) {
-  fragment.push(<PaginationItem id={index}/> )
-}
+  const numbersOfPage = useSelector(
+    (state) => state.numbersOfPage.numbersOfPage
+  );
+  const fragment = [];
+  for (let index = 1; index <= numbersOfPage; index++) {
+    fragment.push(<PaginationItem id={index} />);
+  }
   return (
     <div className={s.container}>
       <div className={s.pagination_container}>
-        <PaginationButton id="0" innerText = '<'/>
-        
-{fragment}
-        <PaginationButton id="5" innerText = '>'/>
+        <PaginationButton id="0" innerText="<" />
+        {fragment}
+        <PaginationButton id="5" innerText=">" />
       </div>
     </div>
   );
